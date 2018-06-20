38c238f79f0c16f2673995f472642a8eda3b3ca0
alex404A <maxmalex1990@gmail.com>
2018-06-20 09:56:35 +0800

    [+] init tag.sh

--- tag.sh
+++ tag.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-latest_branch=$(git describe --tags | grep -Po 'v\d+\.\d+\.\d+')
+prject_name=cmi
+project_dir=$(pwd)
+latest_branch=$(git tag | grep -Po 'v\d+\.\d+\.\d+' | tail -1)
 current_branch=$(git branch | sed -n '/\*/p' | cut -d ' ' -f 2)
 version=$(echo $current_branch | sed -n 's/release-\(v[0-9]\+\.[0-9]\+\.[0-9]\+\)/\1/p')
 if [[ -z "$version" ]]; then
@@ -11,9 +13,20 @@ if [[ -z "$version" ]]; then
 fi
 echo 'gen release.log'
 echo "--$version" > doc/release.log.tmp
-git log $latest_branch...$current_branch --grep Fix: --pretty=format:'%B' >> doc/release.log.tmp
-git log $latest_branch...$current_branch --grep Improve: --pretty=format:'%B' >> doc/release.log.tmp
-git log $latest_branch...$current_branch --grep Feature: --pretty=format:'%B' >> doc/release.log.tmp
+git log $latest_branch...$current_branch --grep "^[+]" --pretty=format:'%B' >> doc/release.log.tmp
+git log $latest_branch...$current_branch --grep "^[-]" --pretty=format:'%B' >> doc/release.log.tmp
+git log $latest_branch...$current_branch --grep "^[!]" --pretty=format:'%B' >> doc/release.log.tmp
+git log $latest_branch...$current_branch --grep "^[*]" --pretty=format:'%B' >> doc/release.log.tmp
+
+cd ./6web
+6web_latest_branch=$(git tag | grep -Po "^$project_name-v\d+\.\d+\.\d+" | tail -1)
+6web_second_latest_branch=$(git tag | grep -Po "^$project_name-v\d+\.\d+\.\d+" | tail -2 | head -1)
+git log $6web_latest_branch...$6web_second_latest_branch --grep "^$project_name[+]" --pretty=format:'%B' >> $project_dir/doc/release.log.tmp
+git log $6web_latest_branch...$6web_second_latest_branch --grep "^$project_name[-]" --pretty=format:'%B' >> $project_dir/doc/release.log.tmp
+git log $6web_latest_branch...$6web_second_latest_branch --grep "^$project_name[!]" --pretty=format:'%B' >> $project_dir/doc/release.log.tmp
+git log $6web_latest_branch...$6web_second_latest_branch --grep "^$project_name[*]" --pretty=format:'%B' >> $project_dir/doc/release.log.tmp
+cd $project_dir
+
 cat doc/release.log.tmp doc/release.log > doc/release.log.new
 rm doc/release.log.tmp && mv doc/release.log.new doc/release.log
 echo 'commit release.log'
@@ -25,3 +38,4 @@ git checkout master && git merge $current_branch
 git push
 git push --tags
 git checkout develop && git merge $current_branch
+git push
